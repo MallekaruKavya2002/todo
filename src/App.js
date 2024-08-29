bbc0dd30b1dc2be7bf975dc751b4ab6df646198e
Mallekaru Kavya <kavyamallekaru@gmail.com>
2024-08-29 21:20:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function Todo(){
+    let[todo,settodo]=useState([
+        {
+            "id":1,
+            "name":"kavya"
+        },
+        {
+            "id":2,
+            "name":"pavan"
+        }
+    ])
 
-export default App;
+    function Addme(){
+        let a = document.getElementById("data1").value
+        let newtodo = [...todo,{"name":a}]
+        settodo(newtodo)
+    }
+    function clearBtn(val){
+        let freshtodo = todo.filter((v)=>{
+            return(
+                v.name !== val
+            )
+        })
+        settodo(freshtodo)
+    }
+    return(
+        <div className="container mt-5 w-50">
+            <div className="input-group ">
+            <input type="text" className="form-control" id="data1"></input> 
+            <button className="btn btn-success" onClick={()=>{Addme()}}>Add me</button>
+            </div>
+            <div>
+                <ul className="list-group">
+                    {
+                        todo.map(
+                            (e)=>{
+                                return(
+                                    <li className="list-group-item mt-2 ">
+                                        <p>{e.name}</p>
+                                        <button className="btn btn-danger" onClick={()=>{clearBtn(e.name)}}>clear</button>
+                                    </li>
+                                )
+                            }
+                        )
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Todo;
